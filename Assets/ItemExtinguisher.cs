76eff0bf8c941977586367fd3da9687164e95c73
hidejiochi <platitude000@gmail.com>
2016-09-20 21:35:51 +0900

    Lesson6　課題提出

---
+++ Assets/ItemExtinguisher.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemExtinguisher : MonoBehaviour {
+
+    private GameObject unitychan;
+
+    private float difference;
+
+	// Use this for initialization
+	void Start () {
+
+        this.unitychan = GameObject.Find("unitychan");
+
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.difference = unitychan.transform.position.z - 10f;
+
+        if (this.transform.position.z < this.difference)
+        {
+            Destroy(gameObject);
+        }
+	
+	}
+}
